99a88c38b6ee8d2404e60d8a792a1587858120df
alexintosh <alessio.d@gmail.com>
2019-04-30 17:41:54 +0200

    Adding more routes

--- server/controllers/UniPool.js
+++ server/controllers/UniPool.js
@@ -4,5 +4,23 @@ module.exports = {
     getAll: async (req, res) => {
         const data = await UniPool.findAll();
         res.send(data);
-    }
+    },
+    byToken: async (req, res) => {
+        console.log('req', req.params);
+        const data = await UniPool.findOne({
+            where: {
+                erc20Address: req.params.tokenAddress
+            }
+        });
+        res.send(data);
+    },
+    byExchangeAddress: async (req, res) => {
+        console.log('req', req.params);
+        const data = await UniPool.findOne({
+            where: {
+                exchangeAddress: req.params.exchangeAddress
+            }
+        }) || {};
+        res.send(data);
+    },
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,4 +2,6 @@ const uniPoolController = require('../controllers').UniPool;
 
 module.exports = (app) => {
     app.get('/api/unipools', uniPoolController.getAll);
+    app.get('/api/unipool/token/:tokenAddress', uniPoolController.byToken);
+    app.get('/api/unipool/exchange/:exchangeAddress', uniPoolController.byExchangeAddress);
 };

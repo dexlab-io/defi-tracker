d198a82dcf1fbfbcd1b48545fefee0ed74b8f6d2
alexintosh <alessio.d@gmail.com>
2019-04-30 18:22:41 +0200

    Adding transpilers for Wallet-core

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const logger = require('morgan');
 const bodyParser = require('body-parser');
 const http = require('http');
 
+import EthereumHDWallet from './lib/ethereum/EthereumHDWallet';
+
 
 
 // Set up the express app
@@ -16,10 +18,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Adding routes
 require('./server/routes')(app);
 
+app.get('/test', async (req, res) => {
+    // console.log('EthereumHDWallet', EthereumHDWallet)
+    const t = new EthereumHDWallet();
+    res.status(200).send({
+        message: await t.web3.eth.getBlock('latest'),
+    })
+});
+
 // Setup a default catch-all route that sends back a welcome message in JSON format.
 app.get('*', (req, res) => res.status(200).send({
     message: 'Suca',
 }));
+
+
 
 const port = parseInt(process.env.PORT, 10) || 8000;
 app.set('port', port);

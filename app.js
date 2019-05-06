81527ebd63214f261a64f5a958354f04aeff0d39
alexintosh <alessio.d@gmail.com>
2019-05-07 01:04:17 +0200

    Fixing a few things

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.get('/test/:name', async (req, res) => {
 
 // Setup a default catch-all route that sends back a welcome message in JSON format.
 app.get('*', (req, res) => res.status(200).send({
-    message: 'Suca',
+    message: 'Hey there 👋',
 }));
 
 
